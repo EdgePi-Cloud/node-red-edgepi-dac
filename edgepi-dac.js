4ad192732d20cdcba1687aa9bb7e723ec7a37234
UBC Student <jtesfu@students.cs.ubc.ca>
2023-10-18 12:22:29 -0700

    Update html.

--- edgepi-dac.js
+++ edgepi-dac.js
@@ -1,84 +1,62 @@
-const { checkValidWriteInput } = require('./helpers/nodeHelpers');
-
 module.exports = function (RED) {
-    const rpc = require('@edgepi-cloud/edgepi-rpc')
-  
-    function DacNode(config) {
-      // Create new node instance with user config
-      RED.nodes.createNode(this, config);
-      const node = this;
-      const ipc_transport = "ipc:///tmp/edgepi.pipe"
-      const tcp_transport = `tcp://${config.tcpAddress}:${config.tcpPort}`
-      const transport = (config.transport === "Network") ? tcp_transport : ipc_transport;
-      const voltage = parseFloat(config.voltage);
-      const channel = rpc.DACChannel[config.dacChannel];
+  const rpc = require("@edgepi-cloud/edgepi-rpc");
 
-      // Init dac
-      const dac = new rpc.DacService(transport);
-  
-      if (dac){
-        console.info("DAC node initialized on:", transport);
-        node.status({fill:"green", shape:"ring", text:"dac initialized"});
-      }
-      else{
-        node.status({fill:"red", shape:"ring", text:"dac failed to initialize."});
-        throw new Error('Failed to initialize dac.')
-      }
+  function DacNode(config) {
+    // Create new node instance with user config
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const ipc_transport = "ipc:///tmp/edgepi.pipe";
+    const tcp_transport = `tcp://${config.tcpAddress}:${config.tcpPort}`;
+    const transport =
+      config.transport === "Network" ? tcp_transport : ipc_transport;
+    const voltage = parseFloat(config.voltage);
+    const channel = rpc.DACChannel[config.dacChannel];
 
-      // Enforce gain on write method and check valid configuration input
-      let validWriteInput;
-      if(config.method === "write"){
-        dac.setDacGain(true,true).then(() => {
-          const upperLim = 10;
-          const lowerLim = 0;
-          validWriteInput = checkValidWriteInput(node, voltage, upperLim, lowerLim);
-          node.on('input',(msg,send,done) => inputListener(msg, send, done));
-        })
-      }
-      else {
-        node.on('input', (msg,send,done) => inputListener(msg, send, done));
-      }
-  
-      async function inputListener(msg,send,done) {{
-        if(config.method === "write" && !validWriteInput){
-          node.status({fill:"red", shape:"dot", text:"node made no call because it has invalid input."});
-          return;
-        }
+    const dac = new rpc.DacService(transport);
 
-        node.status({fill:"green", shape:"dot", text:"input recieved"});
-        try{
-          let response;
-          if(config.method === "read"){
-            const {voltageVal} = await dac.getState({
-              analogOut: channel,
-              voltage: true
-            })
-            response = voltageVal;
-          }
-          else if (config.method === "write"){
-            response = await dac.writeVoltage(channel, voltage);
-            
-          }
-          else {
-            throw new Error(`Unexpected value: ${config.method}`);
-          }
-          msg.payload = response;
+    //  TODO: potentially update if condition based on pos values of dac
+    if (dac) {
+      console.log(config.gainOption);
+      console.log(config.voltage);
+      console.log(config.transport);
+      console.log(config.tcpPort);
+      console.info("DAC node initialized on:", transport);
+      node.status({ fill: "green", shape: "ring", text: "dac initialized" });
+    } else {
+      node.status({
+        fill: "red",
+        shape: "ring",
+        text: "dac failed to initialize.",
+      });
+      throw new Error("Failed to initialize dac.");
+    }
+
+    node.on("input", async function (msg, send, done) {
+      node.status({ fill: "green", shape: "dot", text: "input received" });
+      try {
+        if (config.gain2) {
+          console.log("before set gain");
+          await dac.setDacGain(true, true);
+          console.log("after set gain");
         }
-        
-        catch(error){
-          msg.payload = error;
-          console.error(error)
-        }
-        
-        // Send msg
-        send(msg)
-        
+        console.log("before response");
+        const response = await dac.writeVoltage(channel, voltage);
+        console.log("after response");
+        msg.payload = response;
+      } catch (error) {
+        msg.payload = error;
+        console.error(error);
         if (done) {
-          done();
+          done(error);
         }
-      }}
+      }
+      console.log("before send message");
+      send(msg);
+      console.log("after send message");
+      if (done) {
+        done();
+      }
+    });
   }
-
-    RED.nodes.registerType('edgepi-dac-node', DacNode);
-    
-  };
+  RED.nodes.registerType("dac", DacNode);
+};
